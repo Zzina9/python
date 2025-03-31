1c30e023a3f097da1695e95f5c0527d35c89d60e
Zzina9 <zzina098q@gmail.com>
2025-03-31 18:11:15 +0900

    0331

---
+++ py0331/p0331_01.py
@@ -0,0 +1,50 @@
+# # a_list =[1,2,3,4,5]
+# # sum=0
+# # for i in a_list:
+# #     sum = sum+i
+# # print(sum)
+
+# # a=10
+# # a_list[1,2,3,4,5]
+
+# # print("a :",a)
+# # print("a_list:",a_list)
+
+# # b= a
+# # b=1000
+# # print("a:",a) #10
+# # print("b:",b) #1000
+
+# a_list[1,2,3,4,5]
+# b_list = [*a_list]
+# b_list[1] = 200
+# print(a_list)
+# print(b_list)
+
+
+
+## 주소값 복사: 얕은 복사
+# #a_list,b_list 다른 리스트인가?
+# a_list[1,2,3,4,5]
+# b_list = a_list
+# b_list[1] = 200
+# print(a_list) #[100,2,3,4,5]
+
+
+
+
+
+
+
+
+
+
+
+
+# # 구구단
+# for i in range (2,10):
+#         print("[{}단]".format(i))
+#         for j in range(1,10):
+#             print ("{} X {} ={}".format(i,j,i*j))
+#         print()
+        
---
+++ py0331/p0331_06.py
@@ -0,0 +1,12 @@
+#x좌표:1
+#y좌표:3
+#1,3->문자 인식
+
+num1=int(input("x좌표:"))
+num2=int(input("y좌표:"))
+print(f"[x,y좌표:{num1},{num2}]")
+
+
+num=input("x,y좌표(0,0):")
+n_list=num.split(" ")
+print(f"[x,y좌표:{n_list}]")
